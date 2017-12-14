b333d83c54f70dbd2d23fdb55f4c527958be0da1
Kirill Shestakov <guitarino@yandex.ru>
2017-12-14 02:44:50 -0800

    Adding comment control in the example

--- demo/index.js
+++ demo/index.js
@@ -2,9 +2,11 @@ import { hat } from '../src/hat';
 
 const className = 'className';
 const textContent = 'textContent';
+const commentContent = 'commentContent';
 
 console.log(hat`
   <div class='hat__className--start hat__className--${className} hat__className--end'>
     Text content: ${textContent}
+    <!-- Some comment content: ${commentContent} -->
   </div>
 `);

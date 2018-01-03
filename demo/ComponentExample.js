33e0c20ed8f285f356f897e8bd7703e4bfa53d4a
Kirill Shestakov <guitarino@yandex.ru>
2018-01-03 02:23:15 -0800

    Adding a potential component example and some thought process

---
+++ demo/ComponentExample.js
@@ -0,0 +1,86 @@
+import { hat, Component } from '';
+import Icon from '';
+
+// Good to have `style` properties because
+// the components will be highly reusable and it won't
+// matter if the user has a style-loader. Plus,
+// we'll have dynamic styles. We should have `this.id`
+// for dynamic styling
+
+// `on=${{click, mousedown, etc}}` seems like the shortest
+// syntax for addEventListener. If onclick is an array, it will
+// be treated as [listener, options]
+
+// We have to feed the class through a function, because
+// the user would like to call component like so:
+// hat`<some-html>${ DefaultButton(props) }</some-html>`
+
+// We would also like to provide `hatParentNode` and `hatBoundNodes`
+// properties in case the user would like to do something with those.
+
+export const DefaultButton = hat(class extends Component {
+  constructor() {
+    this.hatParentNode;
+    this.hatBoundNodes;
+  }
+
+  static style() {
+    return hat`
+      .DefaultButton {
+        border: 1px solid black;
+      }
+    `
+  }
+
+  style() {
+    return hat`
+      #${this.id} {
+        color: ${ this.props.color };
+      }
+
+      #${this.id} .DefaultButton__Icon {
+        font-size: ${ this.props.fontSize };
+      }
+    `
+  }
+
+  render() {
+    return hat`
+      <button id=${this.id} class='DefaultButton' on=${{
+        click: this,
+        mousedown: this,
+      }}>
+        ${
+          this.props.hasIcon && hat`
+            <div class='DefaultButton__Icon'>
+              ${ Icon(this.props.iconProps) }
+            </div>
+          `
+        }
+        <div class='DefaultButton__Content'>
+          ${ this.props.children }
+        </div>
+      </button>
+    `
+  }
+
+  willGetProps() {
+    console.log('willGetProps');
+  }
+
+  willRender() {
+    console.log('willRender');
+  }
+
+  didMount() {
+    console.log('didMount');
+  }
+
+  willUnmount() {
+    console.log('willUnmount');
+  }
+
+  handleEvent(e) {
+    console.log('handleEvent', e);
+  }
+})
